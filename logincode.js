61e55d0e20446789f34bfc46f7724502c5b2240d
Uzair Ullah <UzairUllah@corp.contoursoftware.net>
2022-12-20 18:44:23 +0500

    updated

--- logincode.js
+++ logincode.js
@@ -1,22 +1,37 @@
+// Prevent Default behavior of Login form
 const form =document.getElementById('login');
 form.addEventListener('submit', e=>{
     e.preventDefault()  
     console.log("Hello")    
 })
+// Prevent Default behavior of signup form
+const form2=document.querySelector('#signup')
+form2.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    console.log("Hi")
+})
 
+// Show and hide login and signup forms
 const handleSignup=()=>{
-
-    let signupform=document.querySelector('#signup1')
+    let signupform=document.querySelector('#signup')
     let signinform=document.querySelector('#login')
     signinform.style.display='none'
     signupform.style.display='block'
-    signupform.addEventListner('submit', e=>{
-        e.preventDefault()
-        console.log("Helllo")
-    })
 }
-// const signupform1=document.querySelector('#signup1')
-// signupform1.addEventListner('submit', e=>{
-//     e.preventDefault()
-//     console.log("Helllo")
+
+// Get the signup button and add event listener, to hide and show login and sign forms
+// const sign= document.getElementById('handle')
+// console.log(sign.value)
+// sign.addEventListener('submit', ()=>{
+//     let signupform=document.querySelector('#signup1');
+//     let signinform=document.querySelector('#login');
+//     signinform.style.display='none';
+//     signupform.style.display='block';
 // })
+
+// const handleSubmission= (e)=>{
+//     e.preventDefault();
+//     console.log("Hi")
+// }
+
+
