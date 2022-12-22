b88c9c3c39ee4c860eb563ac057fcf22e98d38bf
Uzair Ullah <UzairUllah@corp.contoursoftware.net>
2022-12-22 15:04:47 +0500

    updated

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 // Prevent Default behavior of Login form
 const loginform = document.getElementById('login');
 loginform.addEventListener('submit', e => {
-    e.preventDefault()
-    let name
-    console.log("Hello")
+    e.preventDefault();
+    let loginname = document.querySelector('#loginname');
+    let loginpassword = document.querySelector('#loginpswrd');
+    let logindata={loginname, loginpassword}
+    fetch('http://localhost:8000/users')
+    .then((response)=>{
+        return response.json();
+    })
+    .then(data=>{
+        // console.log(data)
+        for (const user of data){
+            console.log(user)
+            console.log(user.data.name)
+            if (user.data.name===loginname && user.data.password===loginpassword){
+                console.log("Login successful")
+            }
+        }
+    })
+    console.log("Hello");
 })
 // Prevent Default behavior of signup form
-const signupform = document.querySelector('#signup')
+const signupform = document.querySelector('#signup');
 signupform.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log("Hi")
+    console.log("Hi");
     const name = document.querySelector('#name').value;
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
@@ -22,29 +38,29 @@ signupform.addEventListener('submit', (e) => {
         headers: { 'Content-Type': 'application/json' }
     })
         .then((resp) => {
-            console.log("Account Created")
+            console.log("Account Created");
         })
 })
 // Show and hide login and signup forms
-// let signupform=document.querySelector('#signup')
-// let loginform=document.querySelector('#login')
+// let signupform=document.querySelector('#signup');
+// let loginform=document.querySelector('#login');
 let signupbtn = document.querySelector('#signupbtn');
 let loginbtn = document.querySelector('#loginbtn');
 
 signupbtn.addEventListener('click', () => {
-    console.log('Create an account')
-    loginform.style.display = 'none'
-    signupform.style.display = 'block'
-    signupbtn.style.display = 'none'
-    loginbtn.style.display = 'block'
+    console.log('Create an account');
+    loginform.style.display = 'none';
+    signupform.style.display = 'block';
+    signupbtn.style.display = 'none';
+    loginbtn.style.display = 'block';
 })
 
 loginbtn.addEventListener('click', () => {
     console.log('login')
-    loginform.style.display = 'block'
-    signupform.style.display = 'none'
-    signupbtn.style.display = 'block'
-    loginbtn.style.display = 'none'
+    loginform.style.display = 'block';
+    signupform.style.display = 'none';
+    signupbtn.style.display = 'block';
+    loginbtn.style.display = 'none';
 })
 
 
